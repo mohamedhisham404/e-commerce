69931bc82b47a72fb7de1c73fb25b4f0e2211d62
mohamedhisham404 <mohamedhisham694@gmail.com>
2025-01-09 21:08:09 +0200

    coupon routes and controllers

---
+++ backend/routes/coupon.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+    getCoupon,
+    validateCoupon
+} from "../controllers/coupon.controller.js";
+import { protectRoute } from "../middleware/auth.middlware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getCoupon);
+router.post("/validate", protectRoute, validateCoupon);
+
+
+export default router;
