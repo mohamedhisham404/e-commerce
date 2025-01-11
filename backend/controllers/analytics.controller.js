5d247a09bf05ec6afe25fbf662b3c6c5c5e2072e
mohamedhisham404 <mohamedhisham694@gmail.com>
2025-01-11 20:06:33 +0200

    fix getting analysis data error

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,10 +1,10 @@
 import Order from "../models/order.model.js";
 import User from "../models/user.model.js";
+import Product from "../models/product.model.js";
                            
 export const getAnalyticsData = async () => {
     const totalUsers = await User.countDocuments();
     const totalProducts = await Product.countDocuments();
-    
     const salesData = await Order.aggregate([
         {
             $group: {
